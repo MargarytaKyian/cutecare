abe466f8f99b7340e6283c60ede087369c37daf3
MargoKyian <margokyian@gmail.com>
2025-04-10 02:50:07 +0300

    models for users was create

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
                 ('slug', models.SlugField(max_length=100)),
                 ('image', models.ImageField(blank=True, upload_to='products/%Y/%m/%d')),
                 ('description', models.TextField(blank=True)),
-                ('price', models.DecimalField(decimal_places=2, max_digits=10000)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('available', models.BooleanField(default=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('update', models.DateTimeField(auto_now=True)),
