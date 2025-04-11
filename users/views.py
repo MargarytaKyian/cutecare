bd23de62731835cde6e68c6db7a7bd0972b9e5f9
MargoKyian <margokyian@gmail.com>
2025-04-11 05:27:30 +0300

    login, registration and profile was completed

--- users/views.py
+++ users/views.py
@@ -1,34 +1,73 @@
-from django.shortcuts import render
-from django.contrib import auth
+from django.shortcuts import render, redirect
+from django.contrib import auth, messages
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm, \
+    ProfileForm
+from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
+from orders.models import Order, OrderItem
 
 # Create your views here.
 
 def login(request):
+    next_page = request.GET.get('next') or reverse('main:product_list')
+    form = UserLoginForm(data=request.POST or None)
+
     if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username,
-                                     password=password)
-            if user:
+            user = auth.authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            if user is not None:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:product'))
-    else:
-        form = UserLoginForm()
+                return HttpResponseRedirect(next_page)
+            else:
+                messages.error(request, 'Invalid login credentials')
+
     return render(request, 'users/login.html', {'form': form})
 
 
 def registration(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(
+                request, f'{user.username}, Successful Registration'
+            )
+            return HttpResponseRedirect(reverse('user:login'))
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/registration.html', {'form': form})
 
 
+@login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user,
+                           files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile was changed')
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
+    orders = Order.objects.filter(user=request.user).prefetch_related(
+        Prefetch(
+            'items',
+            queryset=OrderItem.objects.select_related('product'),
+        )
+    ).order_by('-id')
+    return render(request, 'users/profile.html',
+                  {'form': form,
+                   'orders': orders})
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:product_list'))
